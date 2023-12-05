63391e62fbb410a3f9d46f7c159fdb5c60574da8
mucunguzi256 <moemucu@gmail.com>
2023-12-05 21:45:51 +0300

    commits

---
+++ meaning.py
@@ -0,0 +1,10 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Write on!')
+# else:
+#     print('Not Today')
+
+# Ternary Operator
+print('Write on!') if meaning > 10 else print('Not Today')
