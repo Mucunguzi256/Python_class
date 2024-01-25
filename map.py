569128ed973862ccbd88f0cb6f630b4f92d30037
mucunguzi256 <moemucu@gmail.com>
2024-01-25 14:09:13 +0300

    filter added

--- map.py
+++ map.py
@@ -3,5 +3,9 @@ my_list = [2,3,4]
 def mult_by2(i):
     return i * 2
 
-print(list(map(mult_by2, my_list)))
+def only_odd(i):
+    return i % 2 != 0
+
 print(my_list)
+print(list(filter(only_odd, my_list)))
+print(list(map(mult_by2, my_list)))
