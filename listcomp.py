06ef0c071f9dc7ad2286adc22cbbbac83322fbdf
mucunguzi256 <moemucu@gmail.com>
2024-01-31 16:48:07 +0300

    list comprehension

--- listcomp.py
+++ listcomp.py
@@ -1,2 +1,8 @@
 my_list = [i for i in 'mucu']
 print(my_list)
+
+num_list = [num for num in range(0,100) if num % 2 == 0]
+print(num_list)
+
+# num2 =  [num*2 for num in range(0,100)]
+# print(num2)
