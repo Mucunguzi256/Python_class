00dbcac9e36f06f505e49ddbf2f9c7497842dde9
mucunguzi256 <moemucu@gmail.com>
2024-02-02 20:44:32 +0300

    fib func added

--- generator.py
+++ generator.py
@@ -7,3 +7,15 @@ next(g)
 next(g)
 next(g)
 print(next(g))
+
+def fib(num):
+    a = 0
+    b = 1
+    for i in range(num):
+        yield a
+        temp = a
+        a = b
+        b = a + b
+
+for x in fib(21):
+    print(x)
