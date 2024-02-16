11b635653eb5245c4f064e9af4e6f479a95e4319
mucunguzi256 <moemucu@gmail.com>
2024-02-16 23:38:06 +0300

    guess num game

--- randomnum.py
+++ randomnum.py
@@ -1,3 +1,16 @@
-import random
+from random import randint
 
-print(random.random(0, 5))
+answer = randint(1, 10)
+
+while True:
+    try:
+        guess = int(input('Guess a number; 1 to 10  '))
+        if 0 < guess < 11:
+            if guess == answer:
+                print("you're a genius")
+                break
+        else:
+            print('I said 1 to 10')
+    except ValueError:
+        print('Please enter a number')
+        continue
